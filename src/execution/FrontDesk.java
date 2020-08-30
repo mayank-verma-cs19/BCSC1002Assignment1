11cbaf66b5bbbd7587e0d29aa20c23c6669a81da
Mayank Verma <67826485+mayank-verma-cs19@users.noreply.github.com>
2020-08-30 17:41:28 +0530

    created a field to issue a new book in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,12 +7,44 @@
 package execution;
 
 import definitions.Book;
+import definitions.Student;
+
+import java.util.Scanner;
 
 public class FrontDesk {
+
+    public static final int ISSUE_A_NEW_BOOK = 1;
+    public static final int RETURN_A_BOOK = 2;
+    public static final int NO_OF_BOOK_ISSUED = 3;
+    public static final int EXIT = 4;
+
     public static void main(String[] args) {
-        Book book = new Book("The Way I Loved her", "Surjeet Dinda", "123456789012");
+        Student student = new Student();
+        Scanner scanner = new Scanner(System.in);
+        int studentInput;
 
-        System.out.println(book);
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            studentInput = scanner.nextInt();
 
+            String isbnNuber;
+            switch (studentInput) {
+                case ISSUE_A_NEW_BOOK:
+                    System.out.println("Please enter the Book name : ");
+                    scanner.nextLine();
+                    String bookName = scanner.nextLine();
+                    System.out.println("Enter the Author name : ");
+                    String authorName = scanner.nextLine();
+                    System.out.println("Enterthe ISBN Number of the book");
+                    String isbnNumber = scanner.nextLine();
+                    Book book = new Book(bookName, authorName, isbnNumber);
+                    break;
+            }
+        } while (studentInput != 4);
     }
 }
